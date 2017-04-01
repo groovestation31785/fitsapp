6444304b171e2258ba6d4daf847dcb8ae43af111
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 15:22:20 -0700

    Generate test helpers and test client model

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -1,9 +1,17 @@
 FactoryGirl.define do
   factory :client do
-    name "MyString"
-    email "MyString"
-    username "MyString"
-    password_hash "MyString"
+    name "Gob Bluth"
+    email "gob@bluth.com"
+    username "gob_bluth"
+    password "magicman"
+    trainer true
+  end
+
+  factory :trainer, class: Client do
+    name "George Michael Bluth"
+    email "gmbluth@phoenix.edu"
+    username "gmbluth"
+    password "annfannmann"
     trainer false
   end
 end
