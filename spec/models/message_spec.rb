5ba34fae109852d4943f662ceb211e1dd87f1dc4
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 15:53:06 -0700

    Add tests for validating messages

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Message, type: :model do
   it { is_expected.to validate_presence_of :content }
   it { is_expected.to validate_presence_of :sender_id }
   it { is_expected.to validate_presence_of :receiver_id }
+  it { is_expected.to validate_numericality_of :sender_id }
+  it { is_expected.to validate_numericality_of :receiver_id }
   it { is_expected.to belong_to :sender }
   it { is_expected.to belong_to :receiver }
 
