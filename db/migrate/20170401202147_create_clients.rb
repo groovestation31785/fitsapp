4b6c5061a7f73a42f6a8ee2c2ba26af91af1e58d
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 15:24:04 -0700

    Migrate and create schema

--- db/migrate/20170401202147_create_clients.rb
+++ db/migrate/20170401202147_create_clients.rb
@@ -4,8 +4,8 @@ class CreateClients < ActiveRecord::Migration[5.0]
       t.string :name
       t.string :email
       t.string :username
-      t.string :password_hash
-      t.boolean :trainer
+      t.string :password_digest
+      t.boolean :trainer, default: false
 
       t.timestamps
     end
