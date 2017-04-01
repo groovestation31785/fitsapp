18faf707c96bfdebaac4a10c068a8b49923b7178
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 16:21:06 -0700

    Update associations to differentiate between sent messages and received messages

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,9 @@
 require 'bcrypt'
 
 class Client < ApplicationRecord
-  has_many :messages
+  has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
+  has_many :received_messages, class_name: "Message", foreign_key: "receiver_id"
+
   validates :name, :email, :username, presence: true
   validates_inclusion_of :trainer, in: [true, false]
   validates :email, :username, uniqueness: true
