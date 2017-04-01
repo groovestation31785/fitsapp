d08bcc01e19323ed54708ec297393f3a923e5a09
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 16:23:34 -0700

    Add tests for client associations

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Client, type: :model do
     it { is_expected.to validate_presence_of :email }
     it { is_expected.to validate_uniqueness_of :username }
     it { is_expected.to validate_uniqueness_of :email }
-    it { is_expected.to have_many :messages }
+    it { is_expected.to have_many :sent_messages }
+    it { is_expected.to have_many :received_messages }
   end
 end
