34ffc1e342536d840c8d7c1652464defbfe9322c
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 15:23:47 -0700

    Add validations to client model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,17 @@
+require 'bcrypt'
+
 class Client < ApplicationRecord
+  validates :name, :email, :username, presence: true
+  validates_inclusion_of :trainer, in: [true, false]
+  validates :email, :username, uniqueness: true
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
