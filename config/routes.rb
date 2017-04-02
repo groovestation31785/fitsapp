022d6245e271438892237a0235bdde7f155563f6
Pat Holland <pholls@users.noreply.github.com>
2017-04-02 14:24:25 -0700

    Refactor client resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :messages, except: [:edit, :update, :destroy]
-  resources :clients, only: [:new, :create]
+  resources :clients, only: [:create]
 
   get    '/signup',  to: 'clients#new'
   get    '/login',   to: 'sessions#new'
