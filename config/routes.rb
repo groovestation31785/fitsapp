d47721bf7e25f192903fa058748b1624d24cb3c1
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 17:56:36 -0700

    Update routes for messages; generate controller specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :messages, except: [:edit, :update, :destroy]
+  get 'signin', to: 'sessions#create', as: 'signin'
+  get 'signout', to: 'sessions#destroy', as: 'signout'
 end
