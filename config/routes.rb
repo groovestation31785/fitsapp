c9517114b74110d37d12c04c914bb74f4b704c60
Pat Holland <pholls@users.noreply.github.com>
2017-04-01 19:53:16 -0700

    Change login and signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'messages#index'
   resources :messages, except: [:edit, :update, :destroy]
-  reources :clients, only: [:new, :create]
-  get 'signin', to: 'sessions#create', as: 'signin'
-  get 'signout', to: 'sessions#destroy', as: 'signout'
+  resources :clients, only: [:new, :create]
+
+  get    '/signup',  to: 'clients#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
